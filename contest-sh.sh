2f2194144a245c8ddb297eea7f53229074dda096
Duy Nguyen <nduytg@gmail.com>
2016-03-28 18:42:13 +0700

    Create contest-sh.sh

---
+++ contest-sh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+terminal='gnome-terminal'
+$terminal -e "sh -c 'curl -v --proxy localhost:8888 http://hcmtec.edu.vn/;exec bash'"&
+$terminal -e "sh -c 'curl -v --proxy localhost:8888 http://hcmtec.edu.vn/;exec bash'"&
+$terminal -e "sh -c 'curl -v --proxy localhost:8888 http://hcmtec.edu.vn/;exec bash'"&
+$terminal -e "sh -c 'curl -v --proxy localhost:8888 http://hcmtec.edu.vn/;exec bash'"&
